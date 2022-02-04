fc924775804458abeeb773d72f99674dd40f7152
pratik <pratikpetkar231@gmail.com>
2022-02-04 09:47:39 +0530

    4feb tried math codes

---
+++ JAVASCRIPT/4FebMath/math.js
@@ -0,0 +1,17 @@
+console.log(Math.round(3.4)); //Returns x rounded to its nearest integer
+console.log(Math.ceil(4.01)); //Math.ceil(x) returns the value of x rounded up to its nearest integer
+console.log(Math.floor(4.9)); //Math.floor(x) returns the value of x rounded down to its nearest integer:
+console.log(Math.trunc(3.9)); //Math.trunc(x) returns the integer part of x:
+console.log(Math.sign(0));    //Math.sign(x) returns if x is negative, null or positive
+console.log(Math.sign(4.8));
+console.log(Math.pow(4,9)); //return value of x with power
+console.log(Math.sqrt(25));
+console.log(Math.abs(-4.7)); //return the same value but always in + manner
+console.log(Math.sin(90 * Math.PI/ 180)); //PI = 22/7
+console.log(Math.cos(0 * Math.PI / 180));
+console.log(Math.min(0, 150, 30, 20, -8, -200));//print minimum value
+console.log(Math.max(0, 150, 30, 20, -8, -200));//print maximum value
+console.log(Math.random());//print random value
+console.log(Math.log(10));//return log value
+console.log(Math.log2(8));
+console.log(Math.log10(1000));
