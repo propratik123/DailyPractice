81aa08fc78664900c666513e5c5b5a80cbb5b512
pratik <pratikpetkar231@gmail.com>
2022-02-21 20:57:59 +0530

    update

--- JAVASCRIPT/project1/project.js
+++ JAVASCRIPT/project1/project.js
@@ -241,3 +241,134 @@ console.log(m);
 console.log(n);
 */
 
+/*
+PROBLEM 13 
+Ajay is given a series(In example).he gone through the series but unable to understand 
+it properly,he has hired you.Your task is to understand the series and print the series 
+2,6,12,20,30... .You are given with a number ‘n’. Find the nth number of series. 
+Input Description: 
+You will be given a number ‘n’
+Output Description: 
+Print the nth number of series
+Sample Input :
+5 
+Sample Output : 
+30
+*/
+var n = 5;
+
+for (let i = 1; i <= n; i++) {
+    var b =( (i * i) + i); //1*1= 1+1 =2 //2*2=4+2=6 //3*3=9+3=12 //4*4=16+4=20 //5*5=25+5=30
+}
+console.log(b);
+/*
+PROBLEM 14 
+You are given with an array. Your task is to print the left rotated array after k 
+opearations. Time:O(n) Extra Space: O(1) 
+Input Description: 
+First line contains two number ‘n’ and ‘k’.Next line contains n space separated 
+numbers.
+Output Description: 
+Print the array as mentioned.
+Sample Input :
+7 3 
+1 2 3 4 5 6 7 
+Sample Output :
+4 5 6 7 1 2 3
+*/
+
+// JavaScript implementation of left rotation of
+// an array K number of times
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let x = arr.length;
+// Function to leftRotate array multiple times
+function leftRotate(arr, x, k) {
+    /* To get the starting point of rotated array */
+    let mod = k % x;
+
+    // Prints the rotated array from start position
+    for (let i = 0; i < x; i++)
+        console.log((arr[(mod + i) % x]) + " ");
+
+}
+
+let k = 3;
+// Function Call
+leftRotate(arr, x, k);
+
+
+
+/*
+ROBLEM 15 
+You are given a postfix expression. Evaluate the given expression and print the result. 
+Input Description: 
+The first line of the input is a string N, containing operators and numbers seperated by 
+the single space which forms a postfix expression.
+Output Description: 
+Evaluate the post expression and print the result.
+Sample Input :
+5 3 1 * + 9 -
+Sample Output :
+-1
+*/
+let exp = "5 3 1 * + 9 -";
+
+function evaluatePostfix(exp) {     // create a stack
+    let stack = [];
+
+    // Scan all characters one by one
+    for (let i = 0; i < exp.length; i++) {
+        let c = exp[i];
+
+        if (c == ' ') {
+            continue;
+        }
+
+        // If the scanned character is an
+        // operand (number here),extract
+        // the number. Push it to the stack.
+        else if (c >= '0' && c <= '9') {
+            let n = 0;
+
+            // extract the characters and
+            // store it in num
+            while (c >= '0' && c <= '9') {
+                n = n * 10 + (c - '0');
+                i++;
+                c = exp[i];
+            }
+            i--;
+
+            // push the number in stack
+            stack.push(n);
+        }
+
+        // If the scanned character is
+        // an operator, pop two elements
+        // from stack apply the operator
+        else {
+            let val1 = stack.pop();
+            let val2 = stack.pop();
+
+            switch (c) {
+                case '+':
+                    stack.push(val2 + val1);
+                    break;
+
+                case '-':
+                    stack.push(val2 - val1);
+                    break;
+
+                case '/':
+                    stack.push(parseInt(val2 / val1, 10));
+                    break;
+
+                case '*':
+                    stack.push(val2 * val1);
+                    break;
+            }
+        }
+    }
+    return stack.pop();
+}
+console.log(evaluatePostfix(exp));
